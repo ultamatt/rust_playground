7723f06b20ee775787eb7af5c3f11790baae8d2d
Matt Moeller <ultamatt@gmail.com>
2021-06-29 16:22:59 +0000

    Finished the lib test

---
+++ 07_lib_test/src/bin/diner.rs
@@ -0,0 +1,8 @@
+use lib_test::eat_at_restaurant as nom_nom;
+use std::collections::HashMap;
+
+fn main(){
+    nom_nom();
+    let mut map = HashMap::new();
+    map.insert(1, 2);
+}
--- 07_lib_test/src/lib.rs
+++ 07_lib_test/src/lib.rs
@@ -1,6 +1,8 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {
+            println!("You're on the waitlist");
+        }
 
         fn seat_at_table() {}
     }
@@ -12,4 +14,60 @@ mod front_of_house {
 
         fn take_payment() {}
     }
+}
+
+mod back_of_house {
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+
+        pub fn say_order(&self){
+            println!("I'd like {} toast please", self.toast);
+        }
+    }
+}
+
+
+
+use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    //crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    //front_of_house::hosting::add_to_waitlist();
+
+    // With the 'use' above
+    hosting::add_to_waitlist();
+
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    meal.say_order();
+
+    //meal.toast = String::from("Cinnamon");
+    //meal.say_order();
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
