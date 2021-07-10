b60e914676b952abf4a5640463091a7b2a711316
Matt Moeller <ultamatt@gmail.com>
2021-07-10 14:54:20 +0000

    Improving my guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,6 +2,25 @@ use std::io; //Use the standard io library
 use std::cmp::Ordering;
 use rand::Rng;
 
+pub struct Guess {
+    value: i32
+}
+
+impl Guess {
+    pub fn new (value:i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess values should be between 1 and 100! We got {}", value);
+            return Guess { value };
+        }
+
+        return Guess { value };
+    }
+
+    pub fn value (&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the Number between 1 and 100!");
 
@@ -20,15 +39,15 @@ fn main() {
             .read_line(&mut guess) //Read into the guess variable, referencing the mutable address
             .expect("Failed to read line");
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
+        let guess: Guess = match guess.trim().parse() {
+            Ok(num) => Guess::new(num),
             Err(_) => continue,
         };
 
-        println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess.value());
         guesses = guesses + 1;
 
-        match guess.cmp(&secret_number) {
+        match guess.value().cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
